01c93b50dbf5575b08104b669751b99b599c6117
Cyphonvoid <yashaswi.kul@gmail.com>
2024-02-08 17:27:43 -0800

    website testing added

--- WebScraper/PageScripts/trulia.py
+++ WebScraper/PageScripts/trulia.py
@@ -8,29 +8,47 @@ from selenium.webdriver.common.keys import Keys
 import time
 driver = webdriver.Chrome()
 
-
-urls = ['https://www.trulia.com/', 'https://www.redfin.com/', 'https://www.compass.com/', 'https://www.realtor.com/', 'https://www.houzeo.com/flat-fee-mls/',
-        'https://vanguardproperties.com/']
+#('https://vanguardproperties.com/', By.XPATH, )
 
 
+urls = [('https://www.trulia.com/', By.XPATH, '/html/body/div[1]/div[1]/main/div[1]/div/div/div[2]/div/div[3]/div/div/div/div/div/div/div/div[1]/div/div/input'),
+        ('https://www.redfin.com/', By.XPATH, '/html/body/div[1]/div[6]/div[2]/div/section/div/div/div/div/div/div/div/div[2]/div/div/form/div/div/input'), 
+        ('https://www.compass.com/', By.XPATH, '/html/body/main/section[1]/div[2]/div/div[2]/div[3]/div/input'), 
+        ('https://www.realtor.com/', By.XPATH, '/html/body/div[1]/div/div[1]/div[3]/div[1]/div/div[1]/div[2]/div/div/div/header/div/div/div/input'), 
+        ('https://www.houzeo.com/flat-fee-mls/', By.XPATH, '/html/body/section[1]/div/div/div/div[1]/div[1]/div[1]/input')
+        ]
+
+
+
+working = []
 #refin, compass, houszero, vanguard   works rest of other sites detect bots. 
 
 driver = webdriver.Chrome()
 
-def load_website(url, times):
+def load_website(url, times, element=None):
+    global works
 
     for i in range(0, times):
         try:
             driver.get(url)
+            if(element != None):
+                searched = driver.find_element(element[0], element[1])
+                works = True
             time.sleep(1)
         except Exception as error:
+            works = False
             print(error)
+        
+    if(works == True):
+        working.append(url)
 
 
 
 for url in urls:
-    load_website(url, 6)
+    load_website(url[0], 3, (url[1], url[2]))
 
+
+print("working urls: ", working)
 driver.quit()
 
 
