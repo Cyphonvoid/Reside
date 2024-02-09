0e7163d0f29cf96ddf251e783b10f23e46325f28
Cyphonvoid <yashaswi.kul@gmail.com>
2024-02-08 17:31:21 -0800

    comments

--- WebScraper/PageScripts/trulia.py
+++ WebScraper/PageScripts/trulia.py
@@ -8,23 +8,24 @@ from selenium.webdriver.common.keys import Keys
 import time
 driver = webdriver.Chrome()
 
-#('https://vanguardproperties.com/', By.XPATH, )
 
 
+#IDEA: Since website detects bot, we can try getting our intended elements, if it detects element won't be present and throw an error and we will know 
+#the website detects, otherwise element is found and we know the website doesn't detect atleast in the number of runs we tested. 
+
+#NOTE: Just add the url of site and a element to this array
 urls = [('https://www.trulia.com/', By.XPATH, '/html/body/div[1]/div[1]/main/div[1]/div/div/div[2]/div/div[3]/div/div/div/div/div/div/div/div[1]/div/div/input'),
         ('https://www.redfin.com/', By.XPATH, '/html/body/div[1]/div[6]/div[2]/div/section/div/div/div/div/div/div/div/div[2]/div/div/form/div/div/input'), 
         ('https://www.compass.com/', By.XPATH, '/html/body/main/section[1]/div[2]/div/div[2]/div[3]/div/input'), 
         ('https://www.realtor.com/', By.XPATH, '/html/body/div[1]/div/div[1]/div[3]/div[1]/div/div[1]/div[2]/div/div/div/header/div/div/div/input'), 
-        ('https://www.houzeo.com/flat-fee-mls/', By.XPATH, '/html/body/section[1]/div/div/div/div[1]/div[1]/div[1]/input')
+        ('https://www.houzeo.com/flat-fee-mls/', By.XPATH, '/html/body/section[1]/div/div/div/div[1]/div[1]/div[1]/input'),
+        ('https://www.zillow.com/homes/for_rent/', By.XPATH, '/html/body/div[1]/div/div[2]/div/section/div[1]/div/form/div/div/input')
         ]
 
-
-
 working = []
-#refin, compass, houszero, vanguard   works rest of other sites detect bots. 
+
 
 driver = webdriver.Chrome()
-
 def load_website(url, times, element=None):
     global works
 
